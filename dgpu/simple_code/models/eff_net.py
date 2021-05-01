2b02ceaf62135ed6454461ca253413533d2c5562
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-05-02 02:19:26 +0530

    dimensions

--- dgpu/simple_code/models/eff_net.py
+++ dgpu/simple_code/models/eff_net.py
@@ -10,8 +10,10 @@ from efficientnet_pytorch import EfficientNet
 class EffNet(nn.Module):
     def __init__(self):
         super(EffNet,self).__init__()
-        self.main_model = EfficientNet.from_pretrained('efficientnet-b0')
+        self.main_model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=1024)
     
     def forward(self,x):
-        x=  self.main_model(x)
+        x = self.main_model(x)
+        x = x.unsqueeze(-1)
+        x = x.unsqueeze(-1)
         return x
