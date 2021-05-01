0539a141982ae9cc0b96c0c5dac3caf73027d481
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-05-02 01:30:04 +0530

    utils.py - Helper functions for building the model and for loading model parameters.    These helper functions are built to mirror those in the official TensorFlow implementation.

--- dgpu/simple_code/models/model.py
+++ dgpu/simple_code/models/model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from .bi_lstm import BiLSTM
 from .mobilenet import MobileNetV1
 from .resnet import resnet50
-from .efficient_net import Efficient_Net
+from .efficient_net import EfficientNet
 
 
 class Model(nn.Module):
@@ -16,7 +16,7 @@ class Model(nn.Module):
         elif args.image_model == 'resnet101':
             self.image_model = resnet101()
         elif args.image_model == 'efficient_net':
-            self.image_model = Efficient_Net()
+            self.image_model = EfficientNet()
 
         # self.bilstm = BiLSTM(args.num_lstm_units, num_stacked_layers = 1, vocab_size = args.vocab_size, embedding_dim = 512)
         self.bilstm = BiLSTM(args)
