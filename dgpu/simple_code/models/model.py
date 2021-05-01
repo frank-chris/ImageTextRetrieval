34b187b0816efaff5b42fa91db94cc3c242a47ba
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-05-02 01:12:24 +0530

    models efficient

--- dgpu/simple_code/models/model.py
+++ dgpu/simple_code/models/model.py
@@ -16,7 +16,7 @@ class Model(nn.Module):
         elif args.image_model == 'resnet101':
             self.image_model = resnet101()
         elif args.image_model == 'efficient_net':
-            self.image_model = EfficientNet()
+            self.image_model = Efficient_Net()
 
         # self.bilstm = BiLSTM(args.num_lstm_units, num_stacked_layers = 1, vocab_size = args.vocab_size, embedding_dim = 512)
         self.bilstm = BiLSTM(args)
