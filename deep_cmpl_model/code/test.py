b5832549378c42891e5640e632be7b31ac99e4d7
raghavgoyal283 <raghavgoyal283@gmail.com>
2021-05-10 21:40:49 +0530

    add

---
+++ deep_cmpl_model/code/test.py
@@ -0,0 +1,170 @@
+import os
+import sys
+import time
+import shutil
+import gc
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.utils.data as data
+
+from utils.helpers import avg_calculator
+from utils.metric import compute_topk, compute_mr
+from utils.directory import makedir, check_file
+
+
+
+from datasets.fashion import Fashion
+from models.model import Model
+
+# from config import data_config, network_config
+
+from test_params import get_test_args
+
+
+def get_metrics(test_loader, network, args):
+
+    batch_time = avg_calculator()
+
+    # switch to evaluate mode
+    network.eval()
+
+    num_samples = args.batch_size * len(test_loader)
+
+    images_bank = torch.zeros((num_samples, args.feature_size))
+    text_bank = torch.zeros((num_samples,args.feature_size))
+    labels_bank = torch.zeros(num_samples)
+
+    index = 0
+    with torch.no_grad():
+
+        timer = time.time()
+
+        for images, captions, labels, captions_length in test_loader:
+
+            test_images = images
+            test_captions = captions
+
+            image_embeddings, text_embeddings = network(test_images, test_captions, captions_length)
+
+            tsize = images.shape[0]
+            
+            images_bank[index: index + tsize] = image_embeddings
+            text_bank[index: index + tsize] = text_embeddings
+            labels_bank[index:index + tsize] = labels
+            index+=tsize
+
+            batch_time.update(time.time() - timer)
+            timer = time.time()
+        
+        images_bank = images_bank[:index]
+        text_bank = text_bank[:index]
+        labels_bank = labels_bank[:index]
+
+        #[t2i_top1, t2i_top10] = compute_topk(text_bank, images_bank, labels_bank, labels_bank, [1,10])
+        #[i2t_top1, i2t_top10] = compute_topk(images_bank, text_bank, labels_bank, labels_bank, [1,10])
+
+        i2t_top1, i2t_top5, i2t_top10, t2i_top1, t2i_top5, t2i_top10 = compute_topk(images_bank, text_bank, labels_bank, labels_bank, [1,5,10], True)
+        i2t_mr, t2i_mr = compute_mr(images_bank, text_bank, labels_bank, labels_bank, 50, True)
+
+
+        return i2t_top1, i2t_top5, i2t_top10, i2t_mr, t2i_top1, t2i_top5, t2i_top10, t2i_mr, batch_time.avg
+
+
+def get_data_loader(image_dir, anno_dir, batch_size, split, max_length):
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    data_split = Fashion(image_dir, anno_dir, split, max_length, test_transform)
+
+    loader = data.DataLoader(data_split, batch_size, shuffle=True, num_workers=2)
+
+    return loader
+
+
+def get_test_model_paths(ckpt_path):
+
+    test_models = os.listdir(ckpt_path)
+    test_models.remove('model_best')
+    test_models = sorted(test_models,key=lambda x: int(x.split(".")[0]))
+    test_models = [os.path.join(ckpt_path,x) for x in test_models]
+    # print(test_models)
+    return test_models
+
+
+def get_network(args, model_path=None):
+
+    network = Model(args)
+    network = nn.DataParallel(network)
+    # cudnn.benchmark = True
+    args.start_epoch = 0
+
+    # process network params
+    if model_path==None:
+        raise ValueError('Supply the model path with --model_path while testing')
+    check_file(model_path, 'model_file')
+    checkpoint = torch.load(model_path)
+    args.start_epoch = checkpoint['epoch'] + 1
+    network_dict = checkpoint['network']
+    network.load_state_dict(network_dict) 
+    print('==> Loading checkpoint "{}"'.format(model_path))
+
+    return network
+
+
+
+def main(args):
+    # need to clear the pipeline
+    # top1 & top10 need to be chosen in the same params ???
+
+    test_loader = get_data_loader(args.image_dir, args.anno_dir, args.batch_size, 'test', args.max_length)
+
+    i2t_top1 = 0.0
+    i2t_top5 = 0.0
+    i2t_top10 = 0.0
+    i2t_mr = 0.0
+
+    t2i_top1 = 0.0
+    t2i_top5 = 0.0
+    t2i_top10 = 0.0
+    t2i_mr = 0.0
+
+    test_models = get_test_model_paths(args.model_path)
+    best_model_path = None
+
+    for model_path in test_models:
+
+        network= get_network(args, model_path)
+
+        i2t_top1_cur, i2t_top5_cur, i2t_top10_cur, i2t_mr_cur, t2i_top1_cur, t2i_top5_cur, t2i_top10_cur, t2i_mr_cur, test_time = get_metrics(test_loader, network, args)
+
+        if t2i_top1_cur > t2i_top1:
+
+            i2t_top1 = i2t_top1_cur
+            i2t_top5 = i2t_top5_cur
+            i2t_top10 = i2t_top10_cur
+            i2t_mr = i2t_mr_cur
+
+            t2i_top1 = t2i_top1_cur
+            t2i_top5 = t2i_top5_cur
+            t2i_top10 = t2i_top10_cur
+            t2i_mr = t2i_mr_cur
+
+            best_model_path = model_path
+            dst_best = os.path.join(args.model_path, 'model_best', 'best.pth.tar')
+            shutil.copyfile(model_path, dst_best)
+
+    
+    # print("Best model: {}".format(best_model_path))
+    print('t2i_top1_best: {:.3f}, t2i_top5_best: {:.3f}, t2i_top10_best: {:.3f}, t2i_mr_best: {:.3f}'.format(t2i_top1, t2i_top5, t2i_top10, t2i_mr))
+    print('i2t_top1: {:.3f}, i2t_top5: {:.3f}, i2t_top10: {:.3f}, i2t_mr_best: {:.3f}'.format(i2t_top1, i2t_top5, i2t_top10, i2t_mr))
+
+
+
+if __name__ == '__main__':
+    
+    args = get_test_args()
+    main(args)
---
+++ deep_cmpl_model/code/train.py
@@ -0,0 +1,230 @@
+import os
+import sys
+import shutil
+import time
+import random
+import numpy as np
+import torch
+import torch.utils.data as data
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+from utils.metric import Loss
+from utils.helpers import avg_calculator
+from utils.directory import makedir, check_file
+
+from datasets.fashion import Fashion
+from models.model import Model
+
+from train_params import get_train_args
+
+
+
+def train(epoch, train_loader, network, optimizer, compute_loss, args):
+
+    # Trains for 1 epoch
+    batch_time = avg_calculator()
+    train_loss = avg_calculator()
+
+    #switch to train mode, req in some modules 
+    network.train()
+
+    end = time.time()
+
+    for step, (images, captions, labels, captions_length) in enumerate(train_loader):
+
+        images = images
+        labels = labels
+        captions = captions
+
+        # compute loss
+        image_embeddings, text_embeddings = network(images, captions, captions_length)
+        cmpm_loss, pos_avg_sim, neg_arg_sim = compute_loss(image_embeddings, text_embeddings, labels)
+        
+
+        if step % 10 == 0:
+            print('epoch:{}, step:{}, cmpm_loss:{:.3f}'.format(epoch, step, cmpm_loss))
+        
+
+        # compute gradient and do ADAM step
+        optimizer.zero_grad()
+        cmpm_loss.backward()
+        #nn.utils.clip_grad_norm(network.parameters(), 5)
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        
+        train_loss.update(cmpm_loss, images.shape[0])
+                
+    return train_loss.avg, batch_time.avg
+
+
+
+
+def get_data_loader(image_dir, anno_dir, batch_size, split, max_length):
+
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    data_split = Fashion(image_dir, anno_dir, split, max_length, train_transform)
+
+    loader = data.DataLoader(data_split, batch_size, shuffle=True, num_workers=2)
+
+    return loader
+
+
+
+def get_network(args, resume=False, model_path=None):
+
+    network = Model(args)
+    network = nn.DataParallel(network)
+    # cudnn.benchmark = True
+    args.start_epoch = 0
+
+    # process network params
+    if resume:
+        if model_path==None:
+            raise ValueError('Supply the model path with --model_path while using ---resume')
+        check_file(model_path, 'model_file')
+        checkpoint = torch.load(model_path)
+        args.start_epoch = checkpoint['epoch'] + 1
+        network_dict = checkpoint['network']
+        network.load_state_dict(network_dict) 
+        print('==> Loading checkpoint "{}"'.format(model_path))
+
+    return network
+
+
+
+def get_optimizer(args, network=None, param=None, resume=False, model_path=None):
+
+    #process optimizer params
+
+    # optimizer
+    # different params for different part
+    cnn_params = list(map(id, network.module.image_model.parameters()))
+    other_params = filter(lambda p: id(p) not in cnn_params, network.parameters())
+    other_params = list(other_params)
+    if param is not None:
+        other_params.extend(list(param))
+    param_groups = [{'params':other_params}, {'params':network.module.image_model.parameters(), 'weight_decay':args.wd}]
+
+    optimizer = torch.optim.Adam(
+        param_groups,
+        lr = args.lr, betas=(args.adam_alpha, args.adam_beta), eps=args.epsilon
+    )
+
+    if resume:
+        check_file(model_path, 'model_file')
+        checkpoint = torch.load(model_path)
+        optimizer.load_state_dict(checkpoint['optimizer'])
+
+    print('Total params: %2.fM' % (sum(p.numel() for p in network.parameters()) / 1000000.0))
+    # seed
+    
+    manualSeed = random.randint(1, 10000)
+    random.seed(manualSeed)
+    np.random.seed(manualSeed)
+    torch.manual_seed(manualSeed)
+    # torch.cuda.manual_seed_all(manualSeed)
+
+    return optimizer
+
+
+
+def adjust_lr(optimizer, epoch, args):
+
+    # Decay learning rate by args.lr_decay_ratio every args.epoches_decay
+
+    if args.lr_decay_type == 'exponential':
+        
+        lr = args.lr * (1 - args.lr_decay_ratio)
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+
+
+def lr_scheduler(optimizer, args):
+
+    if '_' in args.epoches_decay:
+        epoches_list = args.epoches_decay.split('_')
+        epoches_list = [int(e) for e in epoches_list]
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, epoches_list)
+    else:
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, int(args.epoches_decay))
+        
+    return scheduler
+
+
+
+def save_checkpoint(state, epoch, ckpt_dir, is_best):
+
+    filename = os.path.join(ckpt_dir, str(args.start_epoch + epoch)) + '.pth.tar'
+    torch.save(state, filename)
+    if is_best:
+        dst_best = os.path.join(ckpt_dir, 'model_best', str(epoch)) + '.pth.tar'
+        shutil.copyfile(filename, dst_best)
+
+
+
+def main(args):
+    
+    train_loader = get_data_loader(args.image_dir, args.anno_dir, args.batch_size, 'train', args.max_length)
+    
+    # loss
+    compute_loss = Loss(args)
+    nn.DataParallel(compute_loss)
+    
+    # network
+    network = get_network(args, args.resume, args.model_path)
+    optimizer = get_optimizer(args, network, compute_loss.parameters(), args.resume, args.model_path)
+    
+    # lr_scheduler
+    scheduler = lr_scheduler(optimizer, args)
+
+    for epoch in range(args.num_epoches - args.start_epoch):
+        # train for one epoch
+        train_loss, train_time = train(args.start_epoch + epoch, train_loader, network, optimizer, compute_loss, args)
+        # evaluate on validation set
+        print('Train done for epoch-{}'.format(args.start_epoch + epoch))
+
+        
+        state = {'network': network.state_dict(), 
+                    'optimizer': optimizer.state_dict(), 
+                    'W': compute_loss.W, 
+                    'epoch': args.start_epoch + epoch
+                }
+        
+        save_checkpoint(state, epoch, args.checkpoint_dir, False)
+
+        adjust_lr(optimizer, args.start_epoch + epoch, args)
+        scheduler.step()
+
+
+        for param in optimizer.param_groups:
+            print('lr:{}'.format(param['lr']))
+            break
+
+
+
+
+if __name__ == "__main__":
+
+    args = get_train_args()
+
+    if not os.path.exists(args.image_dir):
+        raise ValueError('Supply the dataset directory with --image_dir')
+    if not os.path.exists(args.anno_dir):
+        raise ValueError('Supply the anno file with --anno_dir')
+
+    # save checkpoint
+    makedir(args.checkpoint_dir)
+    makedir(os.path.join(args.checkpoint_dir,'model_best'))
+
+    main(args)
